af403ef7525054303d0e3f107cddbfb74b3a5ecc
Jagiro Elmodovar <jagiro@walla.com>
2021-01-24 19:25:26 +0200

    postgres - use Pool (instead of Client)

--- postgres/index.js
+++ postgres/index.js
@@ -1,50 +1,34 @@
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
-const client = new Client({
+/**
+ * ssl config is required by heroku
+ * https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+ * */
+const sslConfig = process.env.NODE_ENV !== 'production'
+    ? undefined /** local */    
+    : {  /** heroku */
+        rejectUnauthorized: false
+    };
+
+const pool = new Pool({
     /**
      * DATABASE_URL
     *   local: found on ~/.bash_profile
     *   production: provided by heroku
      */
     connectionString: process.env.DATABASE_URL,
-
-    /**
-     * ssl config is required by heroku
-     * https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
-     * */
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: sslConfig,
 })
 
 async function fetchQuotes() {
-    try {
-        await client.connect()
-    }
-    catch (connErr) {
-        console.error('[DB] failed to CONNECT')
-        console.error(connErr)
-        await client.end()
-        throw new Error('Failed to connect to DB')
-    }
-
     let res;
     try {
-        res = await client.query('select * from quotes;')
+        res = await pool.query('select * from quotes;')
     }
     catch (queryErr) {
         console.error('[DB] failed to QUERY for QUOTES')
         console.error(queryErr)
         throw queryErr
-    }
-
-    try {
-        await client.end()
-    }
-    catch(err) {
-        console.error('failed to CLOSE connection')
-        console.error(err)
-        throw new Error('Faild to close DB connection')
     }
 
     const quotes = res ? res.rows.map(row => row.value) : []
